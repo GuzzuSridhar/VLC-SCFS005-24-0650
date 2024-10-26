fc4bb33ea4b19d5aec3e00a73f4d6822690997f3
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-26 09:20:36 +0530

    add singlecatch

---
+++ setwo/src/main/java/com/ntuc/exceptions/SingleCatch.java
@@ -0,0 +1,29 @@
+package com.ntuc.exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SingleCatch {
+    public static int divide(int numerator, int denominator) {
+        return numerator / denominator;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 0, j = 0, res = 0;
+        try {
+            System.out.print("Enter the Numerator: ");
+            i = scan.nextInt();
+            System.out.print("Enter the Denominator: ");
+            j = scan.nextInt();
+            res = divide(i, j);
+            System.out.println(res);
+            // handle more than one exception in a single catch statement
+        } catch (ArithmeticException | InputMismatchException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Closing Scanner");
+            scan.close();
+        }
+    }
+}
