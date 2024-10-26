c646b92a02211763de52597b79e48127dfb594bc
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-26 07:51:46 +0530

    add cmdline args

---
+++ setwo/src/main/java/com/ntuc/cmdlineargs/TestArgs.java
@@ -0,0 +1,18 @@
+package com.ntuc.cmdlineargs;
+
+public class TestArgs {
+    public static void main(String[] args) {
+        for (String string : args) {
+            System.out.println(string);
+        }
+    }
+
+    // void sum() {
+    // int i = 10, j = 20;
+    // System.out.println(i + j);
+    // }
+    // void diff() {
+    // int i = 10, j = 20;
+    // System.out.println(i - j);
+    // }
+}
