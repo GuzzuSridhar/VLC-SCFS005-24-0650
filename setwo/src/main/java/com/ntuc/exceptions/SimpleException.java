45387c233e05391bff25d1cde0bc3d981a91fdda
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-26 08:54:04 +0530

    add exceptions

---
+++ setwo/src/main/java/com/ntuc/exceptions/SimpleException.java
@@ -0,0 +1,23 @@
+package com.ntuc.exceptions;
+
+public class SimpleException {
+    public static int divide(int numerator, int denominator) {
+        return numerator / denominator;
+    }
+
+    public static void main(String[] args) {
+        int i = 10, j = 0, res = 0;
+        try {
+            res = divide(i, j);
+            System.out.println(res);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } finally { // executed in all cases
+            System.out.println("finally executed");
+        }
+    }
+}
+
+/*
+ * to close resources
+ */
