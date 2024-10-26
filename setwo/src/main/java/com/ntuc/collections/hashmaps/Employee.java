ac000e8e28a43587d886aaf21ddcbf2d644ac923
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-26 13:41:30 +0530

    add Employee class

---
+++ setwo/src/main/java/com/ntuc/collections/hashmaps/Employee.java
@@ -0,0 +1,33 @@
+package com.ntuc.collections.hashmaps;
+
+public class Employee {
+    private String name;
+    private String dept;
+
+    public Employee(String name, String dept) {
+        this.name = name;
+        this.dept = dept;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [name=" + name + ", dept=" + dept + "]";
+    }
+
+}
