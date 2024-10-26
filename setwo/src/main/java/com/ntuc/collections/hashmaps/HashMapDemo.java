8ba0a75a5e186acc24f914ddf611c70f195e694d
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-26 13:21:08 +0530

    add hashmap

--- setwo/src/main/java/com/ntuc/collections/hashmaps/HashMapDemo.java
+++ setwo/src/main/java/com/ntuc/collections/hashmaps/HashMapDemo.java
@@ -1,6 +1,8 @@
 package com.ntuc.collections.hashmaps;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class HashMapDemo {
     public static void main(String[] args) {
@@ -12,6 +14,14 @@ public class HashMapDemo {
         nameList.put(2, "Adam");
         nameList.put(3, "Alex");
         nameList.put(4, "Anna");
+
+        // get the iterator on the map
+        Iterator<Map.Entry<Integer, String>> iterator = nameList.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, String> e = iterator.next();
+            System.out.println(e.getKey() + "- " + e.getValue());
+        }
     }
 
 }
