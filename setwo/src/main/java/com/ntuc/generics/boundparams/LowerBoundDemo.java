5070f50c582fb357b230c15f2058cd3076218e49
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-29 17:25:14 +0530

    add comments to lower bound

--- setwo/src/main/java/com/ntuc/generics/boundparams/LowerBoundDemo.java
+++ setwo/src/main/java/com/ntuc/generics/boundparams/LowerBoundDemo.java
@@ -3,24 +3,22 @@ package com.ntuc.generics.boundparams;
 import java.util.List;
 
 public class LowerBoundDemo {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
-            List<Integer> nums = List.of(1, 2, 3, 4);
-            List<Double> dubs = List.of(1.5, 2.5, 3.6, 4.6);
-            List<String> names = List.of("One", "Two", "three");
-    
-            printNums(nums);
-            // printNums(names);
-            // printNums(dubs);
-            
+        List<Integer> nums = List.of(1, 2, 3, 4);
+        List<Double> dubs = List.of(1.5, 2.5, 3.6, 4.6);
+        List<String> names = List.of("One", "Two", "three");
 
-    
+        printNums(nums);
+        // printNums(names);
+        // printNums(dubs);
+
+    }
+
+    static void printNums(List<? super Integer> num) {
+        for (Object n : num) {
+            System.out.println(n);
         }
-    
-        static void printNums(List<? super Integer> num) {
-            for (Object n : num) {
-                System.out.println(n);
-            }
-            System.out.println();
-        }
+        System.out.println();
     }
 }
