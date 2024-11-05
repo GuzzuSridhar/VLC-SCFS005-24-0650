1fc330513c8eadf85f081b1e2fe2da132150eb05
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-05 17:12:32 +0530

    add uid details

--- setwo/src/main/java/com/ntuc/fileio/serialization/Employee.java
+++ setwo/src/main/java/com/ntuc/fileio/serialization/Employee.java
@@ -3,6 +3,12 @@ package com.ntuc.fileio.serialization;
 import java.io.Serializable;
 
 public class Employee implements Serializable {
+
+    // the UID is auto generated if ignored
+    // manual creation of a UID for the objects
+    // can be changed when the class changes to represent changes
+    // private static final long serialVersionUID = 1L;
+
     private int id;
     private String name;
 
