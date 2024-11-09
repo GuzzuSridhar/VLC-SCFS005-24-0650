0e3442dc149ddaab624620f4aec27e4eb5de773a
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-09 08:02:23 +0530

    add insert data

---
+++ setwo/src/main/java/com/ntuc/jdbc/InsertData.java
@@ -0,0 +1,21 @@
+package com.ntuc.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertData {
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcdata", "root", "pass");
+            Statement stmt = conn.createStatement();
+            String sql = "insert into emp(empno,ename) values(100,'Alex')";
+            System.out.println(sql);
+            int res = stmt.executeUpdate(sql);
+            System.out.println(res + " rows inserted");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
