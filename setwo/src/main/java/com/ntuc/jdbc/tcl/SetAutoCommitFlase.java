e1ee605fc76ae545e7ea8692482ef1de6a4cc083
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-11 17:00:31 +0530

    add tcl to jdbc

---
+++ setwo/src/main/java/com/ntuc/jdbc/tcl/SetAutoCommitFlase.java
@@ -0,0 +1,28 @@
+package com.ntuc.jdbc.tcl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SetAutoCommitFlase {
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcdata", "root", "pass");
+            // the autocommit is enabled by default
+            // the same has to be disabled on the connection object
+            conn.setAutoCommit(false);
+            // once the autocommit is set to false
+            // the default action would be a rollback
+
+            Statement stmt = conn.createStatement();
+            int res = stmt.executeUpdate("insert into emp(empno,ename) values(1010,'TestEmployee') ");
+            System.out.println(res + " Records Inserted");
+            conn.commit();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
